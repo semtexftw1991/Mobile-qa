e5af16bf62802e339a24c845bd4b7eaadd0e3c59
Daviditoe <davesimm23@gmail.com>
2020-02-24 19:40:28 -0500

    wip

--- src/mobile/project/DriverSetup.java
+++ src/mobile/project/DriverSetup.java
@@ -1,5 +1,6 @@
 package mobile.project;
 
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
